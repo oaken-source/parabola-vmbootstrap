5cf528fffee270a0b192d2b3c9e1ea97d2173e98
Andreas Grapentin <andreas@grapentin.org>
2018-02-01 19:32:49 +0100

    preparing tarballs from created images

---
+++ make_tarball.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+ ##############################################################################
+ #                       parabola-arm-imagebuilder                            #
+ #                                                                            #
+ #    Copyright (C) 2017  Andreas Grapentin                                   #
+ #                                                                            #
+ #    This program is free software: you can redistribute it and/or modify    #
+ #    it under the terms of the GNU General Public License as published by    #
+ #    the Free Software Foundation, either version 3 of the License, or       #
+ #    (at your option) any later version.                                     #
+ #                                                                            #
+ #    This program is distributed in the hope that it will be useful,         #
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+ #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+ #    GNU General Public License for more details.                            #
+ #                                                                            #
+ #    You should have received a copy of the GNU General Public License       #
+ #    along with this program.  If not, see <http://www.gnu.org/licenses/>.   #
+ ##############################################################################
+
+set -eu
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+[ $(id -u) -ne 0 ] && die "must be root"
+
+_builddir=build
+mkdir -p "$_builddir"
+
+_imagefile=$1
+_rootdir="$_builddir"/root-$$
+
+_loopdev=$(sudo losetup -f --show "$_imagefile")
+sudo partprobe $_loopdev
+
+# register a cleanup error handler
+function cleanup {
+  sudo umount ${_loopdev}p1
+  sudo umount ${_loopdev}p3
+  sudo losetup -d $_loopdev
+  rm -rf "$_rootdir"
+}
+trap cleanup ERR
+
+# mount the image
+mkdir -p "$_rootdir"
+sudo mount -o ro ${_loopdev}p3 "$_rootdir"
+sudo mount -o ro ${_loopdev}p1 "$_rootdir"/boot
+
+tar -czf ParabolaARM-armv7-$(date "+%Y-%m-%d").tar.gz -C "$_rootdir" .
+
+# cleanup
+sudo umount ${_loopdev}p1
+sudo umount ${_loopdev}p3
+sudo losetup -d $_loopdev
+rm -rf "$_rootdir"
+
